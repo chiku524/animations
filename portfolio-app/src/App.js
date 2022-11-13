54861ced3f07f23a08654c7b1ff3bd0563a470dc
Nicholas Chikuji <chiku524@icloud.com>
2022-11-13 01:28:23 -0500

    fixed bubble animation

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -16,7 +16,7 @@ function App() {
       duration: 1,
       repeat: 1,
       color: 'rgb(243, 241, 241)',
-      textShadow: '10px 10px 10px blue',
+      h3Shadow: '10px 10px 10px blue',
       rotateZ: -1,
       yoyo: true
     })
@@ -41,7 +41,7 @@ function App() {
       stagger: { amount: 0.2 }
     })
     .to("#monitor-1, #monitor-2, #monitor-3", {
-      textShadow: '3px 3px red, -1em 0 0.4em olive',
+      h3Shadow: '3px 3px red, -1em 0 0.4em olive',
       opacity: 1,
       duration: 1,
       scale: 1,
@@ -65,11 +65,47 @@ function App() {
       opacity: 0,
       scale: 0,
       transformOrigin: "50% 50%",
-      stagger: {amount: 0.2}
+      stagger: {amount: 0.2},
+      paddingRight: 500
     })
     .from("#person-dog", {
       x: 300,
       duration: 4
+    })
+
+    //anim copy title
+    let animTitle = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".anim-title-container",
+        start: "top center",
+        end: "+=350",
+        markers: true,
+        toggleAttribute: "play none none reverse"
+      } 
+    });
+
+    animTitle
+    .to(".anim-title-container .title", {
+      y: 50,
+      ease: "Power3.easeOut",
+      duration: 0.5
+    })
+    .to("half", {
+      y: 75,
+      ease: "Power3.easeOut",
+      duration: 0.5
+    })
+    .to(".half h3", {
+      stagger: 0.2,
+      y: 25,
+      animationDelay: 2,
+      duration: 0.5,
+      yoyo: true,
+      color: "rgb(243, 241, 241)",
+      textShadow: "2px 2px 10px rgb(5, 108, 226)",
+      boxShadow: "0px 0px 10px #424141",
+      borderRadius: '30%',
+      padding: '10px'
     })
   }, [])
   
@@ -123,16 +159,39 @@ function App() {
       <header className="App-header">
         <div className='header'>
           <h3 className='page-title'>
-            Nico Chikuji's <br />Portfolio site
+            Nico Chikuji's <br />Animations site
           </h3>
         </div>
         <div className='icon-scroll'></div>
         <SVG1 className="eteam-svg"/>
         <section className='dogwalk'>
-          <div className="tank">
-            {bubArray ? bubArray.map((item, index) => <div className={`bubble bubble-${index}`} style={{width: index}}> </div>) : console.log('didnt work')}
+          <div className='tank-container'>
+            <div className="tank">
+              {bubArray ? bubArray.map((item, index) => <div className={`bubble bubble-${index}`} style={{width: index}}> </div>) : console.log('didnt work')}
+            </div>
           </div>
-          <SVG2 className="dogwalk-svg" />
+          <div className='dog-svg-container'>
+            <SVG2 className="dogwalk-svg" />
+          </div>
+          <div className='anim-title-container'>
+            <div className="title">
+              <div className='half first'>
+                <h3>HERE</h3>
+                <h3>ARE</h3>
+                <h3>SOME</h3>
+                <h3>COOL</h3>
+                <h3>ANIMATIONS</h3>
+              </div>
+              <div className='half second'>
+                <h3>I</h3>
+                <h3>HAVE</h3>
+                <h3>CREATED</h3>
+                <h3>FOR</h3>
+                <h3>PRACTICE</h3>
+                <h3>!</h3>
+              </div>
+            </div>
+          </div>
         </section>
       </header>
     </div>
